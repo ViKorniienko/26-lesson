2649be7777ba62dc6406d6bc51bcac2596f2cd8d
ViKorniienko <viko83vk@gmail.com>
2022-01-10 20:48:45 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let a = 0.1;
+let b = 0.2;
+alert(a + b);
+
+let c = "1";
+let d = 2;
+alert(+c + +d);
+
+let sizeFlashDrive = prompt('Enter size your flash drive')
+const sizeFile = 820;
+alert(`Great! Your flash drive can hold ${sizeFlashDrive / sizeFile} files`);
